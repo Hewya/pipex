973f83c5d50eded4fc89e96c6646b96be574f02a
Hewya <gabarnou@gmail.com>
2024-03-21 23:43:13 +0100

    creation du .h et modification des headers

--- pipex.h
+++ pipex.h
@@ -3,10 +3,22 @@
 /*                                                        :::      ::::::::   */
 /*   pipex.h                                            :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: gaelle <gaelle@student.42.fr>              +#+  +:+       +#+        */
+/*   By: gabarnou <gabarnou@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2024/03/21 18:09:47 by gaelle            #+#    #+#             */
-/*   Updated: 2024/03/21 18:54:33 by gaelle           ###   ########.fr       */
+/*   Created: 2024/03/21 18:09:47 by gabarnou          #+#    #+#             */
+/*   Updated: 2024/03/21 23:41:41 by gabarnou         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#ifndef PIPEX_H
+# define PIPEX_H
+
+# include <stdlib.h>
+# include <unistd.h>
+# include <stdio.h>
+# include <fcntl.h>
+# include <string.h>
+# include <sys/wait.h>
+# include "libft/libft.h"
+
+#endif
