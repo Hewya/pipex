ae725c7159e2f6844e577f45d5d198202d715a7a
gabarnou <142003175+gabarnou@users.noreply.github.com>
2024-04-03 21:31:47 +0200

    pipex.c + MaJ .h

--- pipex.c
+++ pipex.c
@@ -6,16 +6,67 @@
 /*   By: gabarnou <gabarnou@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/03/21 18:09:52 by gabarnou          #+#    #+#             */
-/*   Updated: 2024/03/28 01:07:26 by gabarnou         ###   ########.fr       */
+/*   Updated: 2024/04/03 21:31:00 by gabarnou         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "pipex.h"
 
-void	ft_pipex(t_pipex *pipex)
+void	forkchild(t_pipex pipex, int i)
 {
 	
 }
+
+void	ft_pipex(t_pipex *pipex)
+{
+	int	i;
+
+	i = 0;
+	if(pipe(pipex->pipe_fd) == -1)
+	{
+		perror("pipe failed");
+		free_tab(pipex->paths);
+		exit(EXIT_FAILURE);
+	}
+	while (i < pipex->nb_cmds)
+	{
+		forkchild(pipex, i);
+		if (i == 0)
+			pipex->pid_last = pipex->pid;
+		if (pipex->pid != 0)
+			close(pipex->pipe_fd[0]); //fermer lecture
+		i++;
+	}
+	if(pipex->pid != 0) // dans processus parent
+	{
+		close(pipex->pipe_fd[1]); // fermer ecriture parent
+		waitpid(pipex->pid, NULL, 0); // parent attent fin exec prossecus fils
+		waitpid(pipex->pid_last, &pipex->exit_code, 0); // attente du dernier child (bien attendre everyone)
+	}
+}
+
+/*void	ft_fork()
+{
+	pid_t pid = fork();
+	if (pid == -1)
+	{
+		// Il y a une erreur
+		perror("fork");
+		return (EXIT_FAILURE);
+	}
+	else if (pid == 0)
+	{
+		// On est dans le fils
+		printf("Mon PID est %i et celui de mon père est %i\n", getpid(),	getppid());
+	}
+	else
+	{
+		// On est dans le père
+		printf("Mon PID est %i et celui de mon fils est %i\n", getpid(), pid);
+	}
+	return (EXIT_SUCCESS);
+}*/
+
 
 int	main(int ac, char **av, char **envp)
 {
@@ -26,9 +77,10 @@ int	main(int ac, char **av, char **envp)
 	pipex.nb_cmds = ac - 3;
 	pipex.cmds = av;
 	pipex.infile = av[1];
-	pipex.infile_fd = -1; //DOCU LE -1
+	pipex.infile_fd = -1;
 	pipex.outfile = av[ac -1];
 	pipex.outfile_fd = -1;
+	pipex.exit_code = 0;
 	if(ac == 5)
 		ft_pipex(&pipex);
 	else
@@ -37,5 +89,7 @@ int	main(int ac, char **av, char **envp)
 	/*
 	GERER LES EXIT_ERROR 
 	*/
-	return(0); //A CHANGER
+	if(/*quelque chose*/)
+		pipex.exit_code = 127; //command not found
+	return(pipex.exit_code);
 }
--- pipex.h
+++ pipex.h
@@ -6,7 +6,7 @@
 /*   By: gabarnou <gabarnou@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/03/21 18:09:47 by gabarnou          #+#    #+#             */
-/*   Updated: 2024/03/28 00:49:09 by gabarnou         ###   ########.fr       */
+/*   Updated: 2024/04/03 21:29:51 by gabarnou         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -32,6 +32,11 @@ typedef struct s_pipex
 	char	**paths;		// extrait de envp
 	char	**child_args;	// toutes les parties d'une commande
 	char	**envp;			// envp input
+	int		exit_code;		// 
+	int		pipe_fd[2];		// pipes fd : [0] = read, [1] = write
+	int		pid;			// id du fork actuel
+	int		pid_last;		// id de la 1er commande a retourner
+
 
 }			t_pipex;
 
@@ -40,8 +45,14 @@ char	**path_extraction(char **envp);
 char	*ft_strjoin_triple(char const *s1, char const *s2, char const *s3);
 void	ft_execve(t_pipex *pipex);
 
-/*ERRORS*/
+/* ERRORS */
 void	free_tab(char **tab);
 void	ft_command_fail(t_pipex *pipex);
+void	parse_fail(t_pipex *pipex);
+
+/* PIPEX */
+int		main(int ac, char **av, char **envp);
+void	ft_pipex(t_pipex *pipex);
+
 
 #endif
