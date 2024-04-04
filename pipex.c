856342bd156c441aaeea173f76b7bddf6c438000
gabarnou <142003175+gabarnou@users.noreply.github.com>
2024-04-04 15:55:10 +0200

    pipex.c complete

--- pipex.c
+++ pipex.c
@@ -6,15 +6,69 @@
 /*   By: gabarnou <gabarnou@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/03/21 18:09:52 by gabarnou          #+#    #+#             */
-/*   Updated: 2024/04/03 21:31:00 by gabarnou         ###   ########.fr       */
+/*   Updated: 2024/04/04 15:49:51 by gabarnou         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "pipex.h"
 
-void	forkchild(t_pipex pipex, int i)
+void	last_cmd(t_pipex *pipex)
 {
-	
+	close(pipex->pipe_fd[1]);
+	dup2(pipex->pipe_fd[0], STDIN_FILENO);
+	close(pipex->pipe_fd[0]);
+	pipex->outfile_fd = open(pipex->outfile, O_WRONLY | O_TRUNC | O_CREAT, 0644);
+	if(pipex->outfile_fd == -1)
+	{
+		perror("cannot oen output file");
+		free_tab(pipex->child_args);
+		free_tab(pipex->paths);
+		exit(EXIT_FAILURE);
+	}
+	dup2(pipex->outfile_fd, STDOUT_FILENO);
+}
+void	first_cmd(t_pipex *pipex)
+{
+	close(pipex->pipe_fd[0]);
+	dup2(pipex->pipe_fd[1], STDOUT_FILENO);
+	close(pipex->pipe_fd[1]);
+	pipex->infile_fd = open(pipex->infile, O_RDONLY);
+	if(pipex->infile_fd == -1)
+	{
+		perror("cannot open input file");
+		free_tab(pipex->child_args);
+		free_tab(pipex->paths);
+		exit(EXIT_FAILURE);
+	}
+	dup2(pipex->infile_fd, STDIN_FILENO);
+}
+
+void	forkchild(t_pipex *pipex, int i)
+{
+	pipex->pid = fork();
+	if(pipex->pid == -1)
+	{
+		perror("fork failed");
+		free_tab(pipex->paths);
+		exit(EXIT_FAILURE);
+	}
+	if(pipex->pid == 0) // processus fils
+	{
+		pipex->child_args = ft_split(pipex->cmds[pipex->nb_cmds /* + 1 -i*/], ' '); // HERE
+		if(!pipex->child_args)
+		{
+			write(STDERR_FILENO, "pipex : cmd parsing failed", 26);
+			free_tab(pipex->paths);
+			exit(EXIT_FAILURE);
+		}
+		else if (!pipex->child_args[0])
+			command_fail(pipex);
+		if (i == 0)
+			last_cmd(pipex);
+		else if (i == 1)
+			first_cmd;
+		ft_execve(pipex);
+	}
 }
 
 void	ft_pipex(t_pipex *pipex)
@@ -31,9 +85,9 @@ void	ft_pipex(t_pipex *pipex)
 	while (i < pipex->nb_cmds)
 	{
 		forkchild(pipex, i);
-		if (i == 0)
+		if (i == 0) 
 			pipex->pid_last = pipex->pid;
-		if (pipex->pid != 0)
+		if (pipex->pid != 0) // si enfant
 			close(pipex->pipe_fd[0]); //fermer lecture
 		i++;
 	}
@@ -44,7 +98,29 @@ void	ft_pipex(t_pipex *pipex)
 		waitpid(pipex->pid_last, &pipex->exit_code, 0); // attente du dernier child (bien attendre everyone)
 	}
 }
+                              
+int	main(int ac, char **av, char **envp)
+{
+	t_pipex pipex;
 
+	pipex.envp = envp;
+	pipex.paths = path_extraction(envp);
+	pipex.nb_cmds = ac - 3;
+	pipex.cmds = av;
+	pipex.infile = av[1];
+	pipex.infile_fd = -1;
+	pipex.outfile = av[ac -1];
+	pipex.outfile_fd = -1;
+	pipex.exit_code = 0;
+	if(ac == 5)
+		ft_pipex(&pipex);
+	else
+		ft_printf("pipex : input error -> ./pipex infile cmd 1 cmd 2 outfile\n"); //AJOUTER FT_PRINFT A LA LIBFT
+	free_tab(pipex.paths);
+	if(pipex.exit_code >= 256)
+		pipex.exit_code = 127; //command not found
+	return(pipex.exit_code);
+}
 /*void	ft_fork()
 {
 	pid_t pid = fork();
@@ -66,30 +142,3 @@ void	ft_pipex(t_pipex *pipex)
 	}
 	return (EXIT_SUCCESS);
 }*/
-
-
-int	main(int ac, char **av, char **envp)
-{
-	t_pipex pipex;
-
-	pipex.envp = envp;
-	pipex.paths = path_extraction(envp);
-	pipex.nb_cmds = ac - 3;
-	pipex.cmds = av;
-	pipex.infile = av[1];
-	pipex.infile_fd = -1;
-	pipex.outfile = av[ac -1];
-	pipex.outfile_fd = -1;
-	pipex.exit_code = 0;
-	if(ac == 5)
-		ft_pipex(&pipex);
-	else
-		ft_printf("pipex : input error -> ./pipex infile cmd 1 cmd 2 outfile\n"); //AJOUTER FT_PRINFT A LA LIBFT
-	free_tab(pipex.paths);
-	/*
-	GERER LES EXIT_ERROR 
-	*/
-	if(/*quelque chose*/)
-		pipex.exit_code = 127; //command not found
-	return(pipex.exit_code);
-}
--- pipex.h
+++ pipex.h
@@ -6,7 +6,7 @@
 /*   By: gabarnou <gabarnou@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/03/21 18:09:47 by gabarnou          #+#    #+#             */
-/*   Updated: 2024/04/03 21:29:51 by gabarnou         ###   ########.fr       */
+/*   Updated: 2024/04/04 12:24:53 by gabarnou         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -23,19 +23,19 @@
 
 typedef struct s_pipex
 {
+	int		infile_fd;		// identifiant du infile
+	int		outfile_fd;		// identifiant du outfile
 	char	*infile;		// av[1]
 	char	*outfile;		// av[n]
-	int		infile_fd;		//identifiant du infile
-	int		outfile_fd;		//identifiant du outfile
-	char	**cmds;			// av[2] a av[n - 1]
 	int		nb_cmds;		// nombre de commande
-	char	**paths;		// extrait de envp
+	char	**cmds;			// av[2] a av[n - 1]
 	char	**child_args;	// toutes les parties d'une commande
+	char	**paths;		// extrait de envp
 	char	**envp;			// envp input
-	int		exit_code;		// 
 	int		pipe_fd[2];		// pipes fd : [0] = read, [1] = write
 	int		pid;			// id du fork actuel
 	int		pid_last;		// id de la 1er commande a retourner
+	int		exit_code;		// 
 
 
 }			t_pipex;
@@ -53,6 +53,8 @@ void	parse_fail(t_pipex *pipex);
 /* PIPEX */
 int		main(int ac, char **av, char **envp);
 void	ft_pipex(t_pipex *pipex);
-
+void	forkchild(t_pipex *pipex, int i);
+void	first_cmd(t_pipex *pipex);
+void	last_cmd(t_pipex *pipex);
 
 #endif
